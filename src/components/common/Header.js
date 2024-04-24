5f4f3295c43d5074a6fa88f3a440ef9135cc2a2d
Saul <estrellasoscuras1984@gmail.com>
2024-04-24 10:12:32 -0300

    add mockData

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,26 @@
 import React from "react";
+import mockData from '../mockData/mockData.json';
+import LogoSVG from '../assets/Logo.svg'; // Importa el archivo SVG
+import "./Header.css";
 
-export function Header(){
-    return(
-        <>
-        Header
-        </>
-    )
+export function Header() {
+    const { logo, menu, buttons } = mockData.header;
+
+    return (
+        <div className="header-container">
+            <img src={LogoSVG} alt={logo.altText} className="logo"/> {/* Muestra el archivo SVG importado */}
+            <ul className="menu-items">
+                {menu.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+            <div className="button-container">
+                {buttons.map(button => (
+                    <button key={button.id} style={{ backgroundColor: button.color }}>
+                        {button.text}
+                    </button>
+                ))}
+            </div>          
+        </div>
+    );
 }
-
-
--- src/components/common/WrapperContainer.js
+++ src/components/common/WrapperContainer.js
@@ -61,11 +61,11 @@ export function WrapperContainer(){
         <ContanierSlider>
                     <Slider/>
         </ContanierSlider>
-         <ContanierValues>
-                    <Values/>
+         <ContanierValues>         
+                    <Clients/>
          </ContanierValues>
          <ContanierClients>
-                    <Clients/>
+                    <Values/>
          </ContanierClients>
          <ContanierSoon>
                     <Soon/>
